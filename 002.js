5e86047a77efdf1a9ace8b8c19df1cd0d554a34c
Guilqui <guilherme0105@hotmail.com>
2021-08-12 23:34:29 -0300

    Classificando os triangulos pelos seus lados

--- 002.js
+++ 002.js
@@ -1,5 +1,4 @@
 //Classificando um triangulo pelos seus lados
-
 ClasTriang = (a, b, c) => {
     if (a == b && b == c)
     console.log(`Equilátero: Os três lados são iguais, lado 1 = ${a}, lado 2 = ${b}, lado 3 = ${c}.`)
@@ -11,6 +10,7 @@ ClasTriang = (a, b, c) => {
     console.log(`Escaleno: Todos os lados são diferentes, lado 1 = ${a}, lado 2 = ${b}, lado 3 = ${c}.`)
     return a, b, c
 }
+
 
 ClasTriang(1, 1, 1)
 ClasTriang(1, 2, 2)
